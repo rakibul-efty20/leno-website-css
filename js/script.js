68d3142303153288cdf485e87abe42b15e0710ec
efty20 <rakibulhasan52020@gmail.com>
2026-03-09 01:44:36 +0600

    after finishing the all part without the details page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
   // Mobile Menu
   const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
   const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
@@ -6,7 +7,37 @@
     mobileMenu.classList.toggle('active');
   });
 
-  // Navigation background on scroll
+  // Video Modal
+  const modal = document.getElementById('videoModal');
+  const videoButton = document.querySelector('.preview__video-button');
+  const closeButton = document.querySelector('.modal__close-button');
+  const videoPlayer = document.getElementById('videoPlayer');
+
+  // Open modal when clicked
+  videoButton.addEventListener('click', function () {
+    // Show modal
+    modal.style.display = 'block';
+
+    //Replace the src attribute with the video URL
+     videoPlayer.src = 'https://www.youtube.com/embed/8sXRyHI3bLw';
+
+    // Close modal on close button click
+    closeButton.addEventListener('click', function () {
+      modal.style.display = 'none';
+      videoPlayer.src = '';
+    });
+
+    // Close modal on outter click
+    window.addEventListener('click', function (event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+        videoPlayer.src = '';
+      }
+    });
+  });
+});
+
+// Navigation background on scroll
 window.addEventListener('scroll', function () {
   const navbar = document.querySelector('.navbar');
 
