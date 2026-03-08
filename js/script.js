8513f0397ac4ac7fe0740dee1a464dde30f1383e
efty20 <rakibulhasan52020@gmail.com>
2026-03-08 06:01:26 +0600

    after 50% complete

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@
   toggleButton.addEventListener('click', function () {
     mobileMenu.classList.toggle('active');
   });
+
+  // Navigation background on scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar--scroll');
+  } else {
+    navbar.classList.remove('navbar--scroll');
+  }
+});
